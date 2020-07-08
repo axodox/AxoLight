f6e2c22ec6e458a83a973050e10d1381962a9ac3
Peter Major <peter.major@cae.com>
2020-07-09 01:58:37 +0200

    Started calibrator

---
+++ AxoLightCalibrator/AdaLightController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+
+namespace AxoLightCalibrator
+{
+  class AdaLightController
+  {
+    private readonly DataWriter _dataWriter;
+
+    private AdaLightController(DataWriter dataWriter)
+    {
+      _dataWriter = dataWriter;
+    }
+
+    public static async Task<AdaLightController> Create()
+    {
+      var deviceSelector = SerialDevice.GetDeviceSelectorFromUsbVidPid(0x1A86, 0x7523);
+      var deviceInformations = await DeviceInformation.FindAllAsync(deviceSelector);
+      if (deviceInformations.Count == 0) return null;
+
+      var deviceInformation = deviceInformations.FirstOrDefault();
+      var serialDevice = await SerialDevice.FromIdAsync(deviceInformation.Id);
+      serialDevice.BaudRate = 1000000;
+
+      var dataWriter = new DataWriter(serialDevice.OutputStream);
+      return new AdaLightController(dataWriter);
+    }
+
+    public async Task Push(IList<MainPage.RGB> colors)
+    {
+      var messageLength = 6 + colors.Count * 3;
+
+      var stream = new MemoryStream(messageLength);
+      stream.WriteByte(0x41);
+      stream.WriteByte(0x64);
+      stream.WriteByte(0x61);
+
+      var hi = (byte)((colors.Count - 1) >> 8);
+      var lo = (byte)((colors.Count - 1) & 0xff);
+      var checksum = (byte)(hi ^ lo ^ 0x55);
+      stream.WriteByte(hi);
+      stream.WriteByte(lo);
+      stream.WriteByte(checksum);
+      foreach(var color in colors)
+      {
+        stream.WriteByte(ToneMap(color.G));
+        stream.WriteByte(ToneMap(color.R));
+        stream.WriteByte(ToneMap(color.B));
+      }
+
+      _dataWriter.WriteBytes(stream.ToArray());
+      await _dataWriter.StoreAsync();
+    }
+
+    static byte[] _gamma8 = new byte[] {
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+        1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+        2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+        5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+       10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+       17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+       25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+       37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+       51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+       69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+       90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+      115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+      144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+      177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+      215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+    private static byte ToneMap(byte p)
+    {
+      //return (byte)(Math.Pow(2, p / 255.0 * 8) - 1);
+      return _gamma8[p];
+    }
+  }
+}
---
+++ AxoLightCalibrator/MainPage.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Numerics;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AxoLightCalibrator
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page, INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public struct HSL
+    {
+      public float H, S, L;
+    }
+
+    public struct RGB
+    {
+      public byte R, G, B;
+    }
+
+    public SolidColorBrush ReferenceBrush { get; set; }
+
+    private HSL _referenceHSL;
+    private RGB _referenceRGB;
+
+    public RGB TargetRGB => _referenceRGB;
+
+    public float Hue
+    {
+      get => _referenceHSL.H;
+      set
+      {
+        _referenceHSL.H = value;
+        UpdateReferenceColor();
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Hue)));
+      }
+    }
+
+    public float Saturation
+    {
+      get => _referenceHSL.S * 255f;
+      set
+      {
+        _referenceHSL.S = value / 255f;
+        UpdateReferenceColor();
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Saturation)));
+      }
+    }
+
+    public float Lightness
+    {
+      get => _referenceHSL.L * 255f;
+      set
+      {
+        _referenceHSL.L = value / 255f;
+        UpdateReferenceColor();
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Lightness)));
+      }
+    }
+
+    static HSL RgbToHsl(RGB rgb)
+    {
+      HSL result;
+
+      var floatRGB = new Vector3(rgb.R, rgb.G, rgb.B) / 255f;
+
+      var max = Math.Max(Math.Max(floatRGB.X, floatRGB.Y), floatRGB.Z);
+      var min = Math.Min(Math.Min(floatRGB.X, floatRGB.Y), floatRGB.Z);
+
+      var diff = max - min;
+      result.L = (max + min) / 2f;
+      if (Math.Abs(diff) < 0.00001f)
+      {
+        result.S = 0f;
+        result.H = 0f;
+      }
+      else
+      {
+        if (result.L <= 0.5f)
+        {
+          result.S = diff / (max + min);
+        }
+        else
+        {
+          result.S = diff / (2f - max - min);
+        }
+
+        var dist = (new Vector3(max) - floatRGB) / diff;
+
+        if (floatRGB.X == max)
+        {
+          result.H = dist.Z - dist.Y;
+        }
+        else if (floatRGB.Y == max)
+        {
+          result.H = 2f + dist.X - dist.Z;
+        }
+        else
+        {
+          result.H = 4f + dist.Y - dist.X;
+        }
+
+        result.H = result.H * 60f;
+        if (result.H < 0f) result.H += 360f;
+      }
+
+      return result;
+    }
+
+    float QqhToRgb(float q1, float q2, float hue)
+    {
+      if (hue > 360f) hue -= 360f;
+      else if (hue < 0f) hue += 360f;
+
+      if (hue < 60f) return q1 + (q2 - q1) * hue / 60f;
+      if (hue < 180f) return q2;
+      if (hue < 240f) return q1 + (q2 - q1) * (240f - hue) / 60f;
+      return q1;
+    }
+
+    RGB HslToRgb(HSL hsl)
+    {
+      float p2;
+      if (hsl.L <= 0.5f) p2 = hsl.L * (1 + hsl.S);
+      else p2 = hsl.L + hsl.S - hsl.L * hsl.S;
+
+      float p1 = 2f * hsl.L - p2;
+      Vector3 floatRGB;
+      if (hsl.S == 0)
+      {
+        floatRGB = new Vector3(hsl.L);
+      }
+      else
+      {
+        floatRGB = new Vector3(
+          QqhToRgb(p1, p2, hsl.H + 120f),
+          QqhToRgb(p1, p2, hsl.H),
+          QqhToRgb(p1, p2, hsl.H - 120f)
+        );
+      }
+
+      floatRGB *= 255f;
+      return new RGB
+      {
+        R = (byte)floatRGB.X,
+        G = (byte)floatRGB.Y,
+        B = (byte)floatRGB.Z
+      };
+    }
+
+    private void UpdateReferenceColor()
+    {
+      _referenceRGB = HslToRgb(_referenceHSL);
+      ReferenceBrush = new SolidColorBrush(Color.FromArgb(255, _referenceRGB.R, _referenceRGB.G, _referenceRGB.B));
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ReferenceBrush)));
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TargetRGB)));
+    }
+
+    public RGB LedColor;
+
+    public byte Red
+    {
+      get => LedColor.R;
+      set
+      {
+        LedColor.R = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LedColor)));
+      }
+    }
+
+    public byte Green
+    {
+      get => LedColor.G;
+      set
+      {
+        LedColor.G = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LedColor)));
+      }
+    }
+
+    public byte Blue
+    {
+      get => LedColor.B;
+      set
+      {
+        LedColor.B = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LedColor)));
+      }
+    }
+
+    private AdaLightController _controller;
+    private DispatcherTimer _timer;
+
+    public MainPage()
+    {
+      InitializeComponent();
+      Initialize();
+    }
+
+    private async void Initialize()
+    {
+      _controller = await AdaLightController.Create();
+      _timer = new DispatcherTimer();
+      _timer.Tick += OnTimerTick;
+      _timer.Interval = TimeSpan.FromMilliseconds(16);
+      _timer.Start();
+    }
+
+    private async void OnTimerTick(object sender, object e)
+    {
+      var leds = new RGB[156];
+      for(var i = 0; i < leds.Length; i++)
+      {
+        leds[i] = LedColor;
+      }
+      await _controller.Push(leds);
+    }
+  }
+}
